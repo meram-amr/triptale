fc7bb8b2e00dc2fcf1b30b3082e2e97f46f33d9b
meram-amr <meramamr18@gmail.com>
2026-02-28 14:33:17 +0200

    fix navbar href

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ function navbar() {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">TripTales</a>
+                <a className="navbar-brand" href="/">TripTales</a>
                 <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
